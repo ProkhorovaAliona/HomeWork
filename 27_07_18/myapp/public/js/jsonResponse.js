67088f604fbaae0006d068008965bb9ed616e79f
ProkhorovaAliona <aliona.prokhorova@gmail.com>
2018-08-02 23:49:07 +0300

    HW 30_08_18 XHR and Fetch done

--- 27_07_18/myapp/public/js/jsonResponse.js
+++ 27_07_18/myapp/public/js/jsonResponse.js
@@ -1,21 +1,38 @@
+
+/********* XMLHttpRequest ********/
+// const btn = document.querySelector('#getJSON');
+
+// btn.addEventListener('click', function(ev) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'test.json', true);
+//     xhr.send();
+
+//     xhr.onreadystatechange = ()=>{
+//         let responseBody = xhr.responseText;
+//         console.log(responseBody);
+
+//     }
+// });
+
+
+/*********       Fetch       ********/
 const btn = document.querySelector('#getJSON');
 
 btn.addEventListener('click', function(ev) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/test', true);
-    xhr.send();
-
-    xhr.onreadystatechange = ()=>{
-        let responseBody = xhr.responseText;
-        console.log(responseBody);
-
-    }
-
-
-  });
-
-
-
+    'use strict';
+    
+    fetch('/test.json')
+      .then(function(response) {
+        console.log(response.headers.get('Content-Type')); // application/json; charset=utf-8
+        console.log(response.status); // 200
+    
+        return response.json();
+       })
+      .then(function(test) {
+        console.log(test.age); // iliakan
+      })
+      .catch( console.log );
+});
 
  
 
